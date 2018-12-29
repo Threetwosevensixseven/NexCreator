e9d97b6ba734510ef61568803f121fbdf39f1712
Threetwosevensixseven <robin.guest@gmail.com>
2018-12-28 20:47:02 -0500

    Added a C# program to generate the a set of 16K test datafiles to build NexTest.nex with.

---
+++ vs/GenerateMMUBanks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateMMUBanks
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (byte bank16 = 8; bank16 <= 111; bank16++)
+            {
+                var bytes = Enumerable.Repeat(bank16, 0x2000)
+                    .Concat(Enumerable.Repeat(Convert.ToByte(bank16 + 1), 0x2000))
+                    .ToArray();
+                string fn = Path.Combine(BankDirectory, "Bank" + bank16 + ".bin");
+                File.WriteAllBytes(fn, bytes);
+            }
+        }
+
+        private static string BankDirectory
+        {
+            get
+            {
+                return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "..", "nex");
+            }
+        }
+    }
+}
