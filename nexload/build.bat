744c87547d10f39484bd27072b172f83722aeb72
Threetwosevensixseven <robin.guest@gmail.com>
2019-07-24 18:24:03 -0400

    ; v11 24/07/2019 RVG   Now does core and nex version checks before clearing ULA ;                      screen and setting border, and doesn't CLS when returning ;                      custom BASIC errors. This plays nicer with different ;                      layer modes, and is more in line with other dot commands. ; v10 02/06/2019 RVG   Now skips core version check if nextreg 0 reports as an ;                      emulator.
    Updated sjasmplus.

--- nexload/build.bat
+++ nexload/build.bat
@@ -1,11 +1,12 @@
-....:: Set current directory
-::@echo off
+:: Set current directory
+@echo off
 C:
 CD %~dp0
 
 sjasmplus nexload.asm --sym=nexload.sym || goto err
 copy nexload.sna ..\sd\*.*
 copy NEXLOAD. q:\dot\*.*
+copy NEXLOAD. q:\bin\*.*
 :: ..\build\pskill.exe -t cspect.exe
 :: "C:\Program Files (x86)\CSpect1_14\CSpect.exe" -s14 -w3 -zxnext -exit -zx128 -brk -mmc=..\sd\ ..\sd\nexload.sna
 
--- nexload/nexload.asm
+++ nexload/nexload.asm
@@ -5,6 +5,12 @@
 ; Assembles with sjasmplus - https://github.com/z00m128/sjasmplus
 ; 
 ; Changelist:
+; v11 24/07/2019 RVG   Now does core and nex version checks before clearing ULA
+;                      screen and setting border, and doesn't CLS when returning
+;                      custom BASIC errors. This plays nicer with different
+;                      layer modes, and is more in line with other dot commands.
+; v10 02/06/2019 RVG   Now skips core version check if nextreg 0 reports as an
+;                      emulator.
 ; v9  19/02/2019 RVG   If the word at HEADER_FILEHANDLEADDR < $4000, the open 
 ;					   file handle is returned in register C rather than an 
 ;					   address.
@@ -113,6 +119,7 @@ GRAPHIC_OVER_BORDER				= %00000010
 GRAPHIC_SPRITES_VISIBLE			= %00000001
 LORES_ENABLE					= %10000000
 
+NEXT_MACHINE_REGISTER			equ $00
 NEXT_VERSION_REGISTER			equ $01
 CORE_VERSION_REGISTER			equ $0E
 PERIPHERAL_1_REGISTER			equ $05		;Sets joystick mode, video frequency, Scanlines and Scandoubler.
@@ -214,7 +221,8 @@ loadbig
 	ELSE
 	ld	sp,$3fff
 	ENDIF	
-	push ix:call fopen:pop ix	
+	Set14mhz	
+	push ix:call fopen:pop ix
 	call getCurrentCore
 	
 	; set transparency on ULA
@@ -223,15 +231,11 @@ loadbig
 	NEXTREG_nn PALETTE_CONTROL_REGISTER, 0
 	NEXTREG_nn PALETTE_INDEX_REGISTER, 	$18
 	NEXTREG_nn PALETTE_VALUE_REGISTER, 	$e3
-	xor a:out (254),a
-	ld	hl,$5800:ld de,$5801:ld bc,$2ff:ld (hl),0:ldir
+;   xor a:out (254),a
+;   ld hl,$5800:ld de,$5801:ld bc,$2ff:ld (hl),0:ldir
 	ld	bc,4667:ld a,0:out (c),a
 	NEXTREG_nn SPRITE_CONTROL_REGISTER,GRAPHIC_PRIORITIES_SLU + GRAPHIC_SPRITES_VISIBLE
 
-	Set14mhz
-	
-
-	
 ;	ld	a,LAYER_2_PAGE_0*2:NEXTREG_A MMU_REGISTER_6:inc a:NEXTREG_A MMU_REGISTER_7:ld	hl,$c000:ld de,$c001:ld bc,$3fff:ld (hl),l:ldir
 ;	ld	a,LAYER_2_PAGE_1*2:NEXTREG_A MMU_REGISTER_6:inc a:NEXTREG_A MMU_REGISTER_7:ld	hl,$c000:ld de,$c001:ld bc,$3fff:ld (hl),l:ldir
 ;	ld	a,LAYER_2_PAGE_2*2:NEXTREG_A MMU_REGISTER_6:inc a:NEXTREG_A MMU_REGISTER_7:ld	hl,$c000:ld de,$c001:ld bc,$3fff:ld (hl),l:ldir
@@ -255,12 +259,20 @@ loadbig
 	ld	hl,($c000+HEADER_LOADBAR):ld (LoadBar),hl
 	ld	hl,($c000+HEADER_LOADDEL):ld (LoadDel),hl
 	ld	hl,($c000+HEADER_FILEHANDLEADDR):ld (HandleAddr),hl
+	
+    ld a,NEXT_MACHINE_REGISTER
+    ld bc,TBBLUE_REGISTER_SELECT
+    out (c),a:inc b:in a,(c) ; 10 for Next or 8 for emulator
+	cp 8:jp z,.ok            ; Skip core version check if emulator
 
 	ld hl,(CoreMajor):ld de,(CoreSub)
 	ld a,($c000+HEADER_CORE_MAJOR)					:cp l:jr z,.o1:jp nc,coreUpdate:jr .ok
 .o1	ld a,($c000+HEADER_CORE_MINOR)                  :cp h:jr z,.o2:jp nc,coreUpdate:jr .ok
 .o2	ld a,($c000+HEADER_CORE_SUBMINOR)               :cp e:jr z,.ok:jp nc,coreUpdate
 .ok
+
+	xor a:out (254),a
+	ld	hl,$5800:ld de,$5801:ld bc,$2ff:ld (hl),0:ldir
 
 	ld a, ($c000+HEADER_ENTRYBANK):ld (.entryBankSMC),a
 	ld a,($c000+HEADER_DONTRESETNEXTREGS):or a:jp nz,.dontresetregs
@@ -586,7 +598,6 @@ fclose
 ;-------------
 coreUpdate
 	ld sp,(oldStack)
-	call clsWhite
 	ld hl,coretext:call print_rst16	
 	ld hl,(CoreMajor):ld h,0:call dec8:ld a,".":rst 16
 	ld hl,(CoreMinor):ld h,0:call dec8:ld a,".":rst 16
@@ -603,7 +614,6 @@ coreUpdate
 ;-------------
 loaderUpdate
 	ld sp,(oldStack)
-	call clsWhite
 	ld a,($c000+HEADER_VERSION_MAJOR)
 	ld (loadervertextMajor),a
 	ld a,($c000+HEADER_VERSION_MINOR)
@@ -632,12 +642,6 @@ dec0
 	add hl,de
 	rst 16
 	ret
-clsWhite
-	ld	hl,$4000:ld de,$4001:ld bc,$17ff:ld (hl),0:ldir
-	ld	hl,$5800:ld de,$5801:ld bc,$2ff:ld (hl),$38:ldir
-	ld a,22:rst 16:xor a:rst 16:xor a:rst 16
-	ld a,7:out ($fe),a
-	ret	
 
 coretext
 	db	"Your core is ",0
